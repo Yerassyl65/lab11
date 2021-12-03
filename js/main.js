f88d532d1233d911aaeec77eb4ab92157bf9b0d4
Yerassyl65 <91243708+Yerassyl65@users.noreply.github.com>
2021-12-03 20:32:14 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,36 @@ togglePassword.addEventListener('click', function (e) {
     // toggle the eye / eye slash icon
     this.classList.toggle('bi-eye');
 });
+const togglePasswords = document.querySelector('#togglePasswords');
+const confirmpassword = document.querySelector('#confirm_password');
+
+togglePasswords.addEventListener('click', function (a) {
+    // toggle the type attribute
+    const type = confirmpassword.getAttribute('type') === 'password' ? 'text' : 'password';
+    confirmpassword.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
+
+
+function validation(){
+  var input_text = document.querySelector("#input_text");
+  var input_password = document.querySelector("#password");
+  var error = document.querySelector(".error_msg");
+
+  if(input_text.value.length <= 3 || input_password.value.length <= 6 ){
+    input_text.style.border = "1px solid #f74040";
+    input_password.style.border = "1px solid #f74040";
+    error.style.display = "inline-block";
+
+    return false;
+  }
+  else{
+    return true;
+  }
+  
+}
+
+
+
+$("")
